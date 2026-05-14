eb563800a2e01ead80e978e785899e73b2b43030
Isadora Padilha <isap4619@gmail.com>
2026-05-14 20:53:59 -0300

    Adicionado cadastro parcial de equipamento

---
+++ GestaoDeEsquipamentos.ConsoleApp/Dominio/Equipamentos.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GestaoDeEsquipamentos.ConsoleApp.Dominio;
+
+public class Equipamentos
+{
+    public int id;
+    public string nome;
+    public decimal precoAquisicao;
+    public DateTime dataFabricacao;
+}
---
+++ GestaoDeEsquipamentos.ConsoleApp/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GestaoDeEsquipamentos.ConsoleApp.Dominio;
+
+int contadorIds = 1;
+
+Equipamentos[] equipamentosSalvos = new Equipamentos[100];
+
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine("Gestão de Equipamentos");
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine("1 - Cadastrar equipamento");
+    Console.WriteLine("2 - Editar equipamento");
+    Console.WriteLine("3 - Excluir equipamento");
+    Console.WriteLine("4 - Visualizar equipamentos");
+    Console.WriteLine("S - Sair");
+    Console.WriteLine("---------------------------------");
+    Console.Write("> ");
+    string? opcaoMenu = Console.ReadLine()?.ToUpper();
+
+    if (opcaoMenu == "S")
+    {
+        Console.Clear();
+        break;
+    }
+
+    if (opcaoMenu == "1")
+    {
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Cadastro de Equipamentos");
+        Console.WriteLine("---------------------------------");
+        Console.Write("Digite o nome do equipamento: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite o preço de aquisição do equipamento: ");
+        decimal precoAquisicao = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Digite a data de fabriçacão do equipamento: ");
+        DateTime dataFabricacao = DateTime.Parse(Console.ReadLine());
+
+        Equipamentos equipamento = new Equipamentos();
+        equipamento.id = contadorIds++;
+        equipamento.nome = nome;
+        equipamento.precoAquisicao = precoAquisicao;
+        equipamento.dataFabricacao = dataFabricacao;
+
+        for (int i = 0; i < equipamentosSalvos.Length; i++)
+        {
+            if (equipamentosSalvos[i] == null)
+            {
+                equipamentosSalvos[i] = equipamento;
+                break;
+            }
+        }
+
+        Console.WriteLine($"O equipamento {equipamento.nome} foi cadastrado com sucesso!");
+        Console.ReadLine();
+    }
+    else if (opcaoMenu == "2")
+    {
+    }
+    else if (opcaoMenu == "3")
+    {
+    }
+    else if (opcaoMenu == "4")
+    {
+    }
+}
