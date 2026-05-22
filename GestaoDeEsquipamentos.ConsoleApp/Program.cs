958825108e5789b88e7c8fe5b53c22ecc8167685
Isadora Padilha <isap4619@gmail.com>
2026-05-21 21:24:30 -0300

    Cadastro completo

--- GestaoDeEsquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEsquipamentos.ConsoleApp/Program.cs
@@ -8,7 +8,7 @@ Equipamentos[] equipamentosSalvos = new Equipamentos[100];
 
 while (true)
 {
-    Console.Clear();
+    //Console.Clear();
     Console.WriteLine("---------------------------------");
     Console.WriteLine("Gestão de Equipamentos");
     Console.WriteLine("---------------------------------");
@@ -59,13 +59,134 @@ while (true)
         Console.WriteLine($"O equipamento {equipamento.nome} foi cadastrado com sucesso!");
         Console.ReadLine();
     }
+
     else if (opcaoMenu == "2")
     {
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Edição de Equipamentos");
+        Console.WriteLine("---------------------------------");
+
+        Console.WriteLine(
+            "{0, -7} | {1, -15} | {2, -20} | {3, -15}",
+            "Id", "Nome", "Preço de Aquisição", "Data de Fabricação"
+        );
+
+        for (int i = 0; i < equipamentosSalvos.Length; ++i)
+        {
+            Equipamentos eq = equipamentosSalvos[i];
+
+            if (eq == null)
+                continue;
+
+            Console.WriteLine(
+                "{0, -7} | {1, -15} | {2, -20} | {3, -15}",
+                eq.id, eq.nome, eq.precoAquisicao, eq.dataFabricacao
+            );
+        }
+
+        Console.WriteLine("---------------------------------");
+        Console.Write("Digite o id do registro que deseja editar: ");
+        int idSelecionado = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite o nome do equipamento: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite o preço de aquisição do equipamento: ");
+        decimal precoAquisicao = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Digite a data de fabricação do equipamento: ");
+        DateTime dataFabricacao = DateTime.Parse(Console.ReadLine());
+
+        for (int i = 0; i < equipamentosSalvos.Length; i++)
+        {
+            Equipamentos equipamentoSelecionado = equipamentosSalvos[i];
+
+            if (equipamentoSelecionado.id == idSelecionado)
+            {
+                equipamentoSelecionado.nome = nome;
+                equipamentoSelecionado.precoAquisicao = precoAquisicao;
+                equipamentoSelecionado.dataFabricacao = dataFabricacao;
+                break;
+            }
+        }
+
+        Console.WriteLine($"O equipamento {nome} foi editado com suceso!");
+        Console.ReadLine();
     }
+
     else if (opcaoMenu == "3")
     {
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Exclusão de Equipamentos");
+        Console.WriteLine("---------------------------------");
+
+        Console.WriteLine(
+            "{0, -7} | {1, -15} | {2, -20} | {3, -15}",
+            "Id", "Nome", "Preço de Aquisição", "Data de Fabricação"
+            );
+
+        for (int i = 0; i < equipamentosSalvos.Length; i++)
+        {
+            Equipamentos eq = equipamentosSalvos[i];
+
+            if (eq == null)
+                continue;
+
+            Console.WriteLine(
+                "{0, -7} | {1, -15} | {2, -20} | {3, -15}",
+                eq.id, eq.nome, eq.precoAquisicao, eq.dataFabricacao
+            );
+        }
+
+        Console.WriteLine("---------------------------------");
+        Console.Write("Digite o id do registro que deseja excluir: ");
+        int idSelecionado = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < equipamentosSalvos.Length; i++)
+        {
+            Equipamentos equipamentoSelecionado = equipamentosSalvos[i];
+
+            if (equipamentoSelecionado == null)
+                continue;
+
+            if (equipamentoSelecionado.id == idSelecionado)
+            {
+                equipamentosSalvos[i] = null;
+                break;
+            }
+        }
+
+        Console.WriteLine($"O equipamento foi excluído com sucesso!");
+        Console.ReadLine();
     }
+
     else if (opcaoMenu == "4")
     {
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Vizualizacão de Equipamentos");
+        Console.WriteLine("---------------------------------");
+
+        //tabela do console
+        Console.WriteLine(
+            "{0, -7} | {1, -15} | {2, -20} | {3, -15}",
+            "Id", "Nome", "Preco da Aquisicão", "Data de Fabricacão"
+        );
+
+        for (int i = 0; i < equipamentosSalvos.Length; i++)
+        {
+            Equipamentos eq = equipamentosSalvos[i];
+
+            if (eq == null)
+                continue;
+
+            Console.WriteLine(
+                "{0, -7} | {1, -15} | {2, -20} | {3, -15}",
+                eq.id, eq.nome, eq.precoAquisicao, eq.dataFabricacao
+            );
+        }
+
+        Console.WriteLine("---------------------------------");
+        Console.Write("Digite ENTER para continuar...");
+        Console.ReadLine();
     }
 }
